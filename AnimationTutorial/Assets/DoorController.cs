c03cf3de67ab9eb85d5087498947fd3638f07948
EnenAnimation <nnadeau97@gmail.com>
2017-02-28 13:21:37 -0500

    Animation GUI stuff

---
+++ AnimationTutorial/Assets/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+	
+		if (GUILayout.Button ("Open Door")) {
+			animator.SetInteger ("DoorClose", 0);
+			animator.SetInteger ("DoorOpen", 1);
+		}
+
+		if (GUILayout.Button ("Open Close")) {
+			animator.SetInteger ("DoorClose", 1);
+			animator.SetInteger ("DoorOpen", 0);
+		}
+	}
+}
